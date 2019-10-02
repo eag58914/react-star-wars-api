0d047a92831b480f1a02c5240cbb9f2471884e82
Elco Garcia <garciaelco18@gmail.com>
2019-10-02 15:27:38 -0500

    fixed router(thank you andrew!)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { getAllStarships } from '../src/services/starships';
-import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import StarshipPage from './pages/StarShipPage';
 
 class App extends Component {
@@ -16,32 +16,39 @@ class App extends Component {
 	async componentDidMount() {
 		const starships = await getAllStarships();
 		this.setState({ starships: starships.results });
+		console.log(starships.results);
 	}
 
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">STAR WARS STARSHIPS</header>
-				<Switch>
-					<Router
-						exact
-						path="/"
-						render={() => (
-							<section>
-								{this.state.starships.map((starship, idx) => (
-									<Link to={`/starships/${idx}`} key={starship.name}>
-										{starship.name}
-									</Link>
-								))}
-							</section>
-						)}
-					/>
-					<Router
-						path="/starships/:idx"
-						render={(props) => <StarshipPage {...props} getStarship={this.getStarship} />}
-					/>
-				</Switch>
-			</div>
+			<Router>
+				<div className="App">
+					<header className="App-header">STAR WARS STARSHIPS</header>
+
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={() => (
+								<section>
+									{this.state.starships.map((starship, idx) => (
+										<Link to={`/starships/${idx}`} key={starship.name}>
+											{starship.name}
+										</Link>
+									))}{' '}
+									}
+								</section>
+							)}
+						/>
+						<Route
+							path="/starships/:idx"
+							render={(props) => (
+								<StarshipPage {...this.state.starships} getStarship={this.getStarship} />
+							)}
+						/>
+					</Switch>
+				</div>
+			</Router>
 		);
 	}
 }

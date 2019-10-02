d1cab6e3fe9ebe155c572e65b93ef9ca5d33bc63
Elco Garcia <garciaelco18@gmail.com>
2019-10-02 15:28:44 -0500

    added catch to find the error I am getting

---
+++ src/services/starships.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://swapi.co/api/';
+
+export function getAllStarships() {
+	return fetch(`${BASE_URL}starships`, { mode: 'cors' })
+		.then((res) => res.json)
+		.catch((error) => console.error(error));
+}
--- src/services/starships.jsx
+++
@@ -1,5 +0,0 @@
-const BASE_URL = 'https://swapi.co/api/';
-
-export function getAllStarships() {
-	return fetch(`${BASE_URL}starships`, { mode: 'cors' }).then((res) => res.json);
-}
